902f7572fc6ed8586562c3382712b280fc79bfc9
Jean Oldor Saintus <josaintus@gmail.com>
2024-07-15 17:12:40 -0400

    Portfolio en react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+import Home from "./pages/home/Home.jsx";
+import AboutPage from "./pages/aboutPage/AboutPage.jsx";
+import ProjectsPage from "./pages/projectsPage/ProjectsPage.jsx";
+import ContactPage from "./pages/contactPage/ContactPage.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
