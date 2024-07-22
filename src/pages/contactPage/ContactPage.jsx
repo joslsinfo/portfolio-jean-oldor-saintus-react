350472a2ce42be884cac6de8988f759cd98efe9d
Jean Oldor Saintus <josaintus@gmail.com>
2024-07-21 22:44:28 -0400

    Ajout du component ContactPage fonctionnel avec emailjs et update de style dans les différentes parties de l'application

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import "./ContactPage.css";
 import emailjs from "@emailjs/browser";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ContactPage = () => {
   const form = useRef();
@@ -17,6 +19,7 @@ export const ContactPage = () => {
       )
       .then(
         (result) => {
+          toast.success("Votre message a été envoyé avec succès !");
           // Code pour gérer la soumission réussie du formulaire
           console.log("SUCCESS!", result.text);
           // Réinitialiser le formulaire après l'envoi réussi
@@ -25,6 +28,7 @@ export const ContactPage = () => {
           }
         },
         (error) => {
+          toast.error("Une erreur est survenue. Veuillez réessayer plus tard.");
           // Code pour gérer l'échec de la soumission du formulaire
           console.log("FAILED...", error.text);
         }
@@ -32,54 +36,58 @@ export const ContactPage = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className="container mt-5 home">
-      <h1 className="contact">Contact</h1>
-      <p className="contact-message">
-        Merci de visiter mon portfolio ! Que vous ayez des questions, des
-        opportunités professionnelles ou des collaborations à proposer,
-        n'hésitez pas à me contacter. Je vous répondrai dans les plus brefs
-        délais.
-      </p>
+    <div>
+      <ToastContainer />
 
-      <div className="mb-3 mt-3">
-        <label htmlFor="name" className="form-label">
-          Nom
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="your_name"
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Courriel
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="your_email"
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="message" className="form-label">
-          Message
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          className="form-control"
-          required
-        ></textarea>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Send
-      </button>
-    </form>
+      <form ref={form} onSubmit={sendEmail} className="container mt-5 home">
+        <h1 className="contact">Contact</h1>
+        <p className="contact-message">
+          Merci de visiter mon portfolio ! Que vous ayez des questions, des
+          opportunités professionnelles ou des collaborations à proposer,
+          n'hésitez pas à me contacter. Je vous répondrai dans les plus brefs
+          délais.
+        </p>
+
+        <div className="mb-3 mt-3">
+          <label htmlFor="name" className="form-label">
+            Nom
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="your_name"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Courriel
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="your_email"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="form-control"
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
+    </div>
   );
 };
 export default ContactPage;
